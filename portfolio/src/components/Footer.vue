404cefef744cd3a7c59d804d4cecf0a73e9c6f38
icaro baruffi <icaro.baruffi@compasso.com.br>
2020-02-03 13:56:36 -0300

    first commit

---
+++ portfolio/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="Footer">
+  <span>Footer</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ portfolio/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="Header">
+  <span>Header</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
